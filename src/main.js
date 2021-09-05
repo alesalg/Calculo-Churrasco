215d9deef20bfcd446761725ea5ec04c4ac2375e
Alesalg <allesalg@gmail.com>
2021-09-05 13:57:46 -0300

    Alterando CSS e JavaScript

--- src/main.js
+++ src/main.js
@@ -1,21 +1,33 @@
 const botao = document.querySelector("#botaoCalcular")
-botao.addEventListener("click", calcularChurrasco)
 
 const calcularChurrasco = () => {
     const mulheres = document.getElementById('mulheres-qnt').value;
     const homens = document.getElementById('homens-qnt').value;
     const criancas = document.getElementById('criancas-qnt').value;
-    const acompanhamentos = document.getElementById('acompanhamentos').value;
+    const acompanhamentos = document.getElementById('acompanhamentos').checked;
+    const vegetarianos = document.getElementById('vegetariano').checked;
     const bebidasAlcoolicas = document.getElementById('bebidas-alcoolicas').value;
     const bebidasNaoAlcoolicas = document.getElementById('bebidas-nao-alcoolicas').value;
 
-    let totalCarne = (mulheres * 150) + (homens * 200) + (criancas * 100)
-    
-    const totalPessoas = Number(mulheres) + Number(homens) + Number(criancas)
-    const totalAcompanhamento = acompanhamentos ? 50 * Number(totalPessoas) : 0
-    const totalBebidasAlcoolicas = bebidasAlcoolicas ? 500 * Number(bebidasAlcoolicas) : 0
-    const totalBebidasNaoAlcoolicas = bebidasNaoAlcoolicas ? 500 * Number(totalPessoas) : 0
-    
+    // tc = total consumo, tp = total pessoas, ta = total acompanhamentos
+    const tc = Number(mulheres)*150 + Number(homens) * 200 + Number(criancas) * 100
+    const tp = Number(mulheres) + Number(homens) + Number(criancas) 
+    const ta = vegetarianos?(acompanhamentos ? 100 * Number(tp) : 0) : ( acompanhamentos ? 50 * Number(tp) : 0)
+
+    // Logica de valores por grama e multiplicacao da qtde de pessoas
+    let totalCarne = 'Total carne: ${tc}'
+    // Soma da quantidade de pessoas
+    const totalPessoas = 'Total pessoas: ${tp}'
+    // 50g de acompanhamento por pessoa
+    const totalAcompanhamento = 'Total acompanhamento: ${ta}'
+    // 400ml de bebida nao alcoolica para cada pessoa
+    const totalBebidasNaoAlcoolicas = 
+    Number(criancas)>5?('Muita criança para bebida alcoolica!'):(bebidasNaoAlcoolicas ? 'Total bebidas não alcoolicas: ${400 * Number(bebidasNaoAlcoolicas)}' : 'Total bebidas não alcoolicas: ${0}')
+    // 500 ml de bebida alcoolica para cada pessoa
+    const totalBebidasAlcoolicas = bebidasAlcoolicas ? 'Total bebidas alcoolicas: ${500 * NUmber(bebidasAlcoolicas)}' : 'Total bebidas alcoolicas: ${0}'
+
+
+
     document.getElementById("tota-carne").innerHTML = '${getValueMeasure(totalCarne, 1)}'
     document.getElementById("total-pessoas").innerHTML = '${totalPessoas}pessoas'
     document.getElementById("total-acompanhamento").innerHTML = '${getValueMeasure(totalAcompanhamento, 1)}'
@@ -40,3 +52,5 @@ function getValueMeasure(val, type) {
         }
     }
 }
+
+botao.addEventListener("click", calcularChurrasco)
